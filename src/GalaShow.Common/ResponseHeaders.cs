0e6767d6c694ff7c4f41148034216e73cf07e0af
Thedum2 <asdf74591@gmail.com>
2025-09-16 23:11:49 +0900

    refactor(config): template.yaml CORS 설정 간소화 및 ResponseHeaders 정리
    - `template.yaml`에 공통 CORS 설정 추가 (AllowMethods, AllowHeaders, AllowOrigin 등)
    -

--- src/GalaShow.Common/ResponseHeaders.cs
+++ src/GalaShow.Common/ResponseHeaders.cs
@@ -7,12 +7,9 @@ namespace GalaShow.Common
     {
         public static Dictionary<string, string> Get()
         {
-            var origin = Environment.GetEnvironmentVariable("CORS_ALLOWED_ORIGIN") ?? "";
             return new Dictionary<string, string>
             {
-                { "Content-Type", "application/json; charset=utf-8" },
-                { "Access-Control-Allow-Origin", origin },
-                { "Access-Control-Allow-Credentials", "true" }
+                { "Content-Type", "application/json; charset=utf-8" }
             };
         }
     }
