8d53eb402fca4ac4dcb6f333f83796b0a0fd3443
Thedum2 <asdf74591@gmail.com>
2025-09-21 15:52:41 +0900

    feat(question): ChoiceResponse에 ChoiceId 추가

--- src/GalaShow.Common/Models/Response/Question/ChoiceResponse.cs
+++ src/GalaShow.Common/Models/Response/Question/ChoiceResponse.cs
@@ -4,6 +4,9 @@ namespace GalaShow.Common.Models.Response.Question
 {
     public class ChoiceResponse
     {
+        [JsonPropertyName("choiceId")]
+        public int ChoiceId { get; set; }
+
         [JsonPropertyName("text")]
         public string Text { get; set; }
 
